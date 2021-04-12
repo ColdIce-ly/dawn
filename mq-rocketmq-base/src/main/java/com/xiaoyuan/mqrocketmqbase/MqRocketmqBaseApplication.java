b49c28059ca1c6cbe4c3812d3b6696c92b4528ef
liyuan <xiaoyuan.lee@foxmail.com>
2021-04-13 00:44:23 +0800

    rocketmq

---
+++ mq-rocketmq-base/src/main/java/com/xiaoyuan/mqrocketmqbase/MqRocketmqBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyuan.mqrocketmqbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqRocketmqBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqRocketmqBaseApplication.class, args);
+    }
+
+}
---
+++ mq-rocketmq-template/src/main/java/com/xiaoyuan/mqrocketmqtemplate/MqRocketmqTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoyuan.mqrocketmqtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqRocketmqTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqRocketmqTemplateApplication.class, args);
+    }
+
+}
